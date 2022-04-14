9873c713d6b7863c241d309a2566884a52b9cdbc
realguyman <realguyman@protonmail.com>
2022-04-13 20:47:01 -0800

    Pass first test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
+fn quarter_round(a: u32, b: u32, c: u32, d: u32) -> [u32; 4] {
+    [0; 4]
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
     fn quarter_round_test_1() {
         assert_eq!(
             quarter_round(0, 0, 0, 0),
