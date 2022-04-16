93dce84285732d709bcfd1d9fce521b6b9d1c10e
realguyman <realguyman@protonmail.com>
2022-04-15 17:47:57 -0800

    Implement `double_round` function

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,10 @@ fn column_round(mut state: [u32; 16]) -> [u32; 16] {
     state
 }
 
+fn double_round(mut state: [u32; 16]) -> [u32; 16] {
+    row_round(column_round(state))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -161,6 +165,46 @@ mod tests {
                 0x90a20123, 0xead3c4f3, 0x63a091a0, 0xf0708d69,
                 0x789b010c, 0xd195a681, 0xeb7d5504, 0xa774135c,
                 0x481c2027, 0x53a8e4b5, 0x4c1f89c5, 0x3f78c9c8,
+            ]
+        );
+    }
+
+    #[test]
+    fn double_round_test_1() {
+        assert_eq!(
+            double_round(
+                [
+                    0x00000001, 0x00000000, 0x00000000, 0x00000000,
+                    0x00000000, 0x00000000, 0x00000000, 0x00000000,
+                    0x00000000, 0x00000000, 0x00000000, 0x00000000,
+                    0x00000000, 0x00000000, 0x00000000, 0x00000000,
+                ]
+            ),
+            [
+                0x8186a22d, 0x0040a284, 0x82479210, 0x06929051,
+                0x08000090, 0x02402200, 0x00004000, 0x00800000,
+                0x00010200, 0x20400000, 0x08008104, 0x00000000,
+                0x20500000, 0xa0000040, 0x0008180a, 0x612a8020,
+            ]
+        );
+    }
+
+    #[test]
+    fn double_round_test_2() {
+        assert_eq!(
+            double_round(
+                [
+                    0xde501066, 0x6f9eb8f7, 0xe4fbbd9b, 0x454e3f57,
+                    0xb75540d3, 0x43e93a4c, 0x3a6f2aa0, 0x726d6b36,
+                    0x9243f484, 0x9145d1e8, 0x4fa9d247, 0xdc8dee11,
+                    0x054bf545, 0x254dd653, 0xd9421b6d, 0x67b276c1,
+                ]
+            ),
+            [
+                0xccaaf672, 0x23d960f7, 0x9153e63a, 0xcd9a60d0,
+                0x50440492, 0xf07cad19, 0xae344aa0, 0xdf4cfdfc,
+                0xca531c29, 0x8e7943db, 0xac1680cd, 0xd503ca00,
+                0xa74b2ad6, 0xbc331c5c, 0x1dda24c7, 0xee928277,
             ]
         );
     }
